686953af8bd183af32fb93ccf48464dbb2942152
NivethaRajkumar <nivethas1207@gmail.com>
2024-05-26 20:48:22 +0530

    completed

---
+++ index.js
@@ -0,0 +1,22 @@
+//importing space
+import express from "express";
+
+
+//declaration/initialization space
+const app = express();
+const PORT = 4000;
+
+
+//middleware
+app.use(express.json())
+
+//routes
+app.get('/',(req,res)=>{
+    //res.status(200).json({message:"Hi all welcome to our first node app"})
+    res.status(200).send(`<span style="background-color:aqua;color:black;font-size:100px">Welcome to our first nodejs app</span>`)
+})
+
+
+app.listen(PORT,()=>{
+    console.log(`App is listening on the port ${PORT}`);
+})
